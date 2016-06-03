a5d75bbb3b51512cb2966391c1a712253bdf2f19
fanborges <fanborges@gmail.com>
2016-06-03 13:18:44 -0500

    Delete ESPECIALIDAD_HORAS.sql

--- entregables/VISTAS/ESPECIALIDAD_HORAS.sql
+++
@@ -1,9 +0,0 @@
---Vista ESPECIALIDAD_HORAS
---Muestra las horas de consulta por especialidad.
---Soluciona el requerimiento número 2: Total en horas de consulta realizadas por especialidad.
-CREATE OR REPLACE VIEW ESPECIALIDAD_HORAS
-AS
-SELECT E.NOMBRE , SUM(I.DURACION) HORAS FROM INFORMACION_COMPLETA I 
-INNER JOIN ESPECIALIDAD E
-ON I.IDESPECIALIDAD=E.IDESPECIALIDAD
-GROUP BY E.NOMBRE;
